9a0c44197199d19ce767a22a9ceb76a97ad0ea48
tona0516 <tona.meem@gmail.com>
2014-11-30 11:49:20 +0900

    Revert "Revert "first""
    This reverts commit 5055f8dec2882e40f141c8e337c749dca55c1b12.

---
+++ src/com/tona/test/readingdifficultkanji/RuleDisplay.java
@@ -0,0 +1,14 @@
+package com.tona.test.readingdifficultkanji;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RuleDisplay extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_rule);
+	}
+}
